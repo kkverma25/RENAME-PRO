eb665a0629f49dfcf06ebf8aaf189d5c97c06512
Mr.vermaji <85053195+kkverma25@users.noreply.github.com>
2022-06-15 20:44:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -13,10 +13,10 @@ async def start(client,message):
 
 ➠ 𝘐 𝘊𝘢𝘯 𝘙𝘦𝘯𝘢𝘮𝘦 ✍️ 𝘈𝘯𝘺 𝘍𝘪𝘭𝘦 🗃️ & 𝘝𝘪𝘥𝘦𝘰 📸 𝘞𝘪𝘵𝘩 𝘊𝘶𝘴𝘵𝘰𝘮 𝘛𝘩𝘶𝘮𝘣𝘯𝘢𝘪𝘭 𝘚𝘶𝘱𝘱𝘰𝘳𝘵.
        
-➠ 𝘔𝘢𝘪𝘯𝘵𝘢𝘪𝘯𝘦𝘥 𝘉𝘺 : @ChVivekTomar
+➠ 𝘔𝘢𝘪𝘯𝘵𝘢𝘪𝘯𝘦𝘥 𝘉𝘺 : @D_E_V_l_L
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("About Me 🇮🇳" ,url="https://t.me/ItsAll_AboutMe") ]  ]))
+	 [[ InlineKeyboardButton("About Me 🇮🇳" ,url="https://t.me/About_EVERETT") ]  ]))
 	
 
 
